65e3bf2c543eb913bbd24262e0702767a32bdc11
Leandro <mangicl@gmail.com>
2022-10-26 20:09:51 -0300

    Programa de bulones - Primer etapa en python

---
+++ módulo1.py
@@ -0,0 +1,12 @@
+#variableDiametro
+diametro = 6, 7, 8, 9, 10
+#variablePaso
+paso = 1, 1.25, 1.5
+#variableLargo
+largo = 1/4, 1/2, 3/4, 1
+#variableRosca
+rosca = "rosca izquierda", "rosca derecha"
+#variableCliente
+"import"
+#fechaEntrega
+"calendar"
