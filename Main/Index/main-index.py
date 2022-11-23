e6fa5e83b8e477175c42d12004f720b904f25528
Leandro <115804454+mangicl@users.noreply.github.com>
2022-11-23 20:27:04 -0300

    Bucle para continuar comprando bulones

--- Main/Index/main-index.py
+++ Main/Index/main-index.py
@@ -14,15 +14,12 @@ from tkinter import N, Y
 import Calendario
 
 #main - resources
-
+ingresarMedidas = Y,N
+continuar = Y,N
 diametro = 6, 7, 8, 9, 10, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 52, 56, 60, 64, 68, 72, 76, 80
-
 paso = 1, 1.25, 1.5, 2, 2.25, 3, 3.5, 4, 4.5, 5, 5.5, 6
-
 largo = 1/4, 1/2, 3/4, 1
-
 rosca = "izquierda", "derecha"
-
 cantidad = "unidad", "kilo"
 
 #index
@@ -42,49 +39,53 @@ cantidad = "unidad", "kilo"
 #cantidad = input ("Indique cantidad")
 #print("usted a ingresado: " + cantidad)
 
-
 while True:
-    diametro = input("Ingrese el diametro")
-    if diametro != "6":
-        print("Introduzca un diametro valido")
-    else:
-        print("Correcto")
-        break
-    continue
+    ingresarMedidas = input("¿Desea comenzar?")
+    if ingresarMedidas == Y:
+        print("Ingrese las medidas a continuación:")
+        while True:
+            diametro = input("Ingrese el diametro")
+            if diametro != "6":
+                print("Introduzca un diametro valido")
+            else:
+                print("Correcto")
+                break
+            continue
 
-while True:
-    paso = input("Ingrese el paso")
-    if paso != "1":
-        print("Introduzca un paso valido")
-    else:
-        print("Correcto")
-        break
-    continue
+        while True:
+                paso = input("Ingrese el paso")
+                if paso != "1":
+                    print("Introduzca un paso valido")
+                else:
+                    print("Correcto")
+                    break
+                continue
 
-while True:
-    largo = input("Ingrese el largo")
-    if largo != "1/4":
-        print("Introduzca un largo valido")
-    else:
-        print("Correcto")
-        break
-    continue
+        while True:
+            largo = input("Ingrese el largo")
+            if largo != "1/4":
+                print("Introduzca un largo valido")
+            else:
+                print("Correcto")
+                break
+            continue
 
-while True:
-    rosca = input("Ingrese el tipo de rosca (izquierda/derecha)")
-    if rosca != "derecha":
-        print("Introduzca un tipo de rosca valido")
-    else:
-        print("Correcto")
-        break
-    continue
+        while True:
+            rosca = input("Ingrese el tipo de rosca (izquierda/derecha)")
+            if rosca != "derecha":
+                print("Introduzca un tipo de rosca valido")
+            else:
+                print("Correcto")
+                break
+            continue
 
-while True:
-    cantidad = input("Indique la cantidad (unidad/kilo)")
-    if cantidad != "unidad":
-        print("Indique una cantidad valida")
-    else:
-        print("Correcto")
-        break
-    continue
+        while True:
+            cantidad = input("Indique la cantidad (unidad/kilo)")
+            if cantidad != "unidad":
+                print("Indique una cantidad valida")
+            else:
+                print("Correcto")
+                break
+            continue
+
 
