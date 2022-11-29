2514cd7787438dc665a7e0bbe26da4bf21dc3a89
Leandro <mangicl@gmail.com>
2022-11-28 21:02:56 -0300

    Modificacion y agregado de mas datos

--- Main/Index/Programa_Bulunes.py
+++ Main/Index/Programa_Bulunes.py
@@ -11,7 +11,7 @@ from ast import While
 #from cgitb import reset
 from operator import concat
 from tkinter import N, Y
-#import Calendario
+import Calendario
 # libreria para excel
 import openpyxl
 from openpyxl import workbook
@@ -32,11 +32,11 @@ deseaComenzar = Y,N
 continuar = Y,N
 seleccion = 0
 #diametro_stock = 6, 7, 8, 9, 10, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 52, 56, 60, 64, 68, 72, 76, 80
-diametro_stock = "6"
+diametro_stock = 0
 #paso_stock = 1, 1.25, 1.5, 2, 2.25, 3, 3.5, 4, 4.5, 5, 5.5, 6
-paso_stock = "1"
+paso_stock = 0
 #largo_stock = 1/4, 1/2, 3/4, 1
-largo_stock = "1/4"
+largo_stock = 0
 cantidad1 = "unidad"
 cantidad2 = "kilo"
 
@@ -88,8 +88,8 @@ while True:
                 print(" ")
 
                 while True:
-                    diametro_input = input("Ingrese el diametro en milimetros: ")
-                    if diametro_input != diametro_stock:
+                    diametro_input = int(input("Ingrese el diametro en milimetros: "))
+                    if diametro_input != 6 and diametro_input != 7 and diametro_input != 8 and diametro_input != 9 and diametro_input != 10 and diametro_input != 16 and diametro_input != 18 and diametro_input != 20 and diametro_input != 22 and diametro_input != 24 and diametro_input != 27 and diametro_input != 30 and diametro_input != 33 and diametro_input != 36 and diametro_input != 39 and diametro_input != 42 and  diametro_input != 45 and diametro_input != 48 and diametro_input != 52 and diametro_input != 56 and diametro_input != 60 and diametro_input != 64 and diametro_input != 68 and diametro_input != 72 and diametro_input != 76 and diametro_input != 80:
                         print("Introduzca un diametro valido")
                     else:
                         print("Diametro valido")
@@ -97,8 +97,8 @@ while True:
                     continue
 
                 while True:
-                    paso_input = input("Ingrese el paso: ")
-                    if paso_input != paso_stock:
+                    paso_input = float(input("Ingrese el paso: "))
+                    if paso_input != 1 and paso_input != 1.25 and paso_input != 1.5 and paso_input != 2 and paso_input != 2.25 and paso_input != 3 and paso_input != 3.5 and paso_input != 4 and paso_input != 4.5 and paso_input != 5 and paso_input != 5.5 and paso_input != 6:
                         print("Introduzca un paso valido")
                     else:
                         print("Paso valido")
@@ -107,7 +107,7 @@ while True:
 
                 while True:
                     largo_input = input("Ingrese el largo en pulgadas: ")
-                    if largo_input != largo_stock:
+                    if largo_input != "1/4" and largo_input != "1/2" and largo_input != "3/4" and largo_input != "1":
                         print("Introduzca un largo valido")
                     else:
                         print("Largo valido")
@@ -134,8 +134,8 @@ while True:
                 print(" ")
 
                 while True:
-                    diametro_input = input("Ingrese el diametro en milimetros: ")
-                    if diametro_input != "6":
+                    diametro_input = int(input("Ingrese el diametro en milimetros: "))
+                    if diametro_input != 6 and diametro_input != 7 and diametro_input != 8 and diametro_input != 9 and diametro_input != 10 and diametro_input != 16 and diametro_input != 18 and diametro_input != 20 and diametro_input != 22 and diametro_input != 24 and diametro_input != 27 and diametro_input != 30 and diametro_input != 33 and diametro_input != 36 and diametro_input != 39 and diametro_input != 42 and  diametro_input != 45 and diametro_input != 48 and diametro_input != 52 and diametro_input != 56 and diametro_input != 60 and diametro_input != 64 and diametro_input != 68 and diametro_input != 72 and diametro_input != 76 and diametro_input != 80:
                         print("Introduzca un diametro valido")
                     else:
                         print("Diametro valido")
@@ -143,8 +143,8 @@ while True:
                     continue
 
                 while True:
-                    paso_input = input("Ingrese el paso: ")
-                    if paso != "1":
+                    paso_input = float(input("Ingrese el paso: "))
+                    if paso_input != 1 and paso_input != 1.25 and paso_input != 1.5 and paso_input != 2 and paso_input != 2.25 and paso_input != 3 and paso_input != 3.5 and paso_input != 4 and paso_input != 4.5 and paso_input != 5 and paso_input != 5.5 and paso_input != 6:
                         print("Introduzca un paso valido")
                     else:
                         print("Paso valido")
@@ -153,7 +153,7 @@ while True:
 
                 while True:
                     largo_input = input("Ingrese el largo en pulgadas: ")
-                    if largo != "1/4":
+                    if largo_input != "1/4" and largo_input != "1/2" and largo_input != "3/4" and largo_input != "1":
                         print("Introduzca un largo valido")
                     else:
                         print("Largo valido")
