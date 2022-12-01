ab2c2f94da0feff9c151e0b303d20ff222416756
Leandro <115804454+mangicl@users.noreply.github.com>
2022-12-01 19:17:34 -0300

    Cambios en el menu y codigo del programa bulones

--- Main/Index/Programa_Bulunes.py
+++ Main/Index/Programa_Bulunes.py
@@ -37,14 +37,18 @@ diametro_stock = 0
 paso_stock = 0
 #largo_stock = 1/4, 1/2, 3/4, 1
 largo_stock = 0
-cantidad1 = "unidad"
-cantidad2 = "kilo"
+rosca_izq = "izquierda"
+rosca_der = "derecha"
+unidad1 = "unidad"
+unidad2 = "kilo"
 
 #Variable de contabilidad
+cantUnidad = 0
+cantKilos = 0
 cuentaUnidad = 0
-cuentaUnidadTotal = cuentaUnidad
+cuentaUnidadTotal = (cuentaUnidad + cantUnidad)
 cuentaKilos = 0
-cuentaKilosTotal = cuentaKilos
+cuentaKilosTotal = (cuentaKilos + cantKilos)
 cuentaCantidadTotal = cuentaUnidadTotal + (cuentaKilosTotal * 100000)
 
 #index
@@ -66,7 +70,7 @@ cuentaCantidadTotal = cuentaUnidadTotal + (cuentaKilosTotal * 100000)
 
 while True:
     deseaComenzar = input("¿Desea comenzar/continuar? (Y para SI / N para NO)")
-    if deseaComenzar != N:
+    if deseaComenzar == Y:
         print("Ingrese las datos a continuación:")
     elif deseaComenzar == N:
         print("")
@@ -75,107 +79,17 @@ while True:
         break
     
     while True:
-       if seleccion != 6:
+        if seleccion != 6:
             print("""Seleccione el tipo de operacion:
-            1) Bulones de rosca derecha
-            2) Bulones de rosca izquierda
-            3) Consultar stock disponible
+            1) Consultar stock disponible
+            2) Seleccionar bulon a comprar
+            3) Ver totales de la compra
             4) Seguir comprando
-            5) Salir""")
+            5) Borrar datos ingresados y volver a comenzar
+            6) Salir""")
             seleccion = int(input("Elija una opcion: "))
+
             if seleccion == 1:
-                print(" ")
-                print("Rosca derecha seleccionada")
-                print(" ")
-
-                while True:
-                    diametro_input = int(input("Ingrese el diametro en milimetros: "))
-                    if diametro_input != 6 and diametro_input != 7 and diametro_input != 8 and diametro_input != 9 and diametro_input != 10 and diametro_input != 16 and diametro_input != 18 and diametro_input != 20 and diametro_input != 22 and diametro_input != 24 and diametro_input != 27 and diametro_input != 30 and diametro_input != 33 and diametro_input != 36 and diametro_input != 39 and diametro_input != 42 and  diametro_input != 45 and diametro_input != 48 and diametro_input != 52 and diametro_input != 56 and diametro_input != 60 and diametro_input != 64 and diametro_input != 68 and diametro_input != 72 and diametro_input != 76 and diametro_input != 80:
-                        print("Introduzca un diametro valido")
-                    else:
-                        print("Diametro valido")
-                        break
-                    continue
-
-                while True:
-                    paso_input = float(input("Ingrese el paso: "))
-                    if paso_input != 1 and paso_input != 1.25 and paso_input != 1.5 and paso_input != 2 and paso_input != 2.25 and paso_input != 3 and paso_input != 3.5 and paso_input != 4 and paso_input != 4.5 and paso_input != 5 and paso_input != 5.5 and paso_input != 6:
-                        print("Introduzca un paso valido")
-                    else:
-                        print("Paso valido")
-                        break
-                    continue
-
-                while True:
-                    largo_input = input("Ingrese el largo en pulgadas: ")
-                    if largo_input != "1/4" and largo_input != "1/2" and largo_input != "3/4" and largo_input != "1":
-                        print("Introduzca un largo valido")
-                    else:
-                        print("Largo valido")
-                        break
-                    continue
-
-                while True:
-                    cantidad_input = input("Indique si compra por UNIDAD o por KILO): ")
-                    if cantidad_input != cantidad1:
-                        print("Indique una cantidad valida")
-                    else:
-                        print("Correcto")
-                        break    
-                    if cantidad_input != cantidad2:
-                        print("Indique una cantidad valida")
-                    else:
-                        print("Correcto")    
-                        break
-                    continue
-
-            if seleccion == 2:
-                print(" ")
-                print("Rosca izquierda seleccionada")
-                print(" ")
-
-                while True:
-                    diametro_input = int(input("Ingrese el diametro en milimetros: "))
-                    if diametro_input != 6 and diametro_input != 7 and diametro_input != 8 and diametro_input != 9 and diametro_input != 10 and diametro_input != 16 and diametro_input != 18 and diametro_input != 20 and diametro_input != 22 and diametro_input != 24 and diametro_input != 27 and diametro_input != 30 and diametro_input != 33 and diametro_input != 36 and diametro_input != 39 and diametro_input != 42 and  diametro_input != 45 and diametro_input != 48 and diametro_input != 52 and diametro_input != 56 and diametro_input != 60 and diametro_input != 64 and diametro_input != 68 and diametro_input != 72 and diametro_input != 76 and diametro_input != 80:
-                        print("Introduzca un diametro valido")
-                    else:
-                        print("Diametro valido")
-                        break
-                    continue
-
-                while True:
-                    paso_input = float(input("Ingrese el paso: "))
-                    if paso_input != 1 and paso_input != 1.25 and paso_input != 1.5 and paso_input != 2 and paso_input != 2.25 and paso_input != 3 and paso_input != 3.5 and paso_input != 4 and paso_input != 4.5 and paso_input != 5 and paso_input != 5.5 and paso_input != 6:
-                        print("Introduzca un paso valido")
-                    else:
-                        print("Paso valido")
-                        break
-                    continue
-
-                while True:
-                    largo_input = input("Ingrese el largo en pulgadas: ")
-                    if largo_input != "1/4" and largo_input != "1/2" and largo_input != "3/4" and largo_input != "1":
-                        print("Introduzca un largo valido")
-                    else:
-                        print("Largo valido")
-                        break
-                    continue
-
-                while True:
-                    cantidad_input = input("Indique si compra por UNIDAD o por KILO): ")
-                    if cantidad_input != cantidad1:
-                        print("Indique una cantidad valida")
-                    else:
-                        print("Correcto")
-                        break    
-                    if cantidad_input != cantidad2:
-                        print("Indique una cantidad valida")
-                    else:
-                        print("Indique a continuación cuantos kilos quiere comprar")    
-                        break
-                    continue
-
-            elif seleccion == 3:
                 print(" ")
                 print("Usted ha elegido consultar el stock disponible")
                 print(" ")
@@ -188,12 +102,103 @@ while True:
                     {bulones[3]} con rosca izquierda y {bulones[4]} con rosca derecha""")
                 break
 
+            if seleccion == 2:
+                print(" ")
+                print("Comencemos a ingresar los datos del bulon...")
+                print(" ")
+
+                while True:
+                    diametro_input = int(input("Ingrese el diametro en milimetros: "))
+                    if diametro_input != 6 and diametro_input != 7 and diametro_input != 8 and diametro_input != 9 and diametro_input != 10 and diametro_input != 16 and diametro_input != 18 and diametro_input != 20 and diametro_input != 22 and diametro_input != 24 and diametro_input != 27 and diametro_input != 30 and diametro_input != 33 and diametro_input != 36 and diametro_input != 39 and diametro_input != 42 and  diametro_input != 45 and diametro_input != 48 and diametro_input != 52 and diametro_input != 56 and diametro_input != 60 and diametro_input != 64 and diametro_input != 68 and diametro_input != 72 and diametro_input != 76 and diametro_input != 80:
+                        print("Introduzca un diametro valido")
+                    else:
+                        print("Diametro valido")
+                        break
+                    continue
+
+                while True:
+                    paso_input = input("Ingrese el paso: ")
+                    if paso_input != "1" and paso_input != "1.25" and paso_input != "1.5" and paso_input != "2" and paso_input != "2.25" and paso_input != "3" and paso_input != "3.5" and paso_input != "4" and paso_input != "4.5" and paso_input != "5" and paso_input != "5.5" and paso_input != "6":
+                        print("Introduzca un paso valido")
+                    else:
+                        print("Paso valido")
+                        break
+                    continue
+
+                while True:
+                    largo_input = input("Ingrese el largo en pulgadas: ")
+                    if largo_input != "1/4" and largo_input != "1/2" and largo_input != "3/4" and largo_input != "1":
+                        print("Introduzca un largo valido")
+                    else:
+                        print("Largo valido")
+                        break
+                    continue
+
+                while True:
+                    rosca_input = input("Ingrese el tipo de rosca (IZQUIERDA / DERECHA): ")
+                    if rosca_input != rosca_izq and rosca_input != rosca_der:
+                        print("Introduzca un tipo de rosca valido")
+                    else:
+                        print("Rosca valida")
+                        break
+                    continue
+
+                while True:
+                    unidad_input = input("Indique si compra por UNIDAD o por KILO): ")
+                    if unidad_input != unidad1 and unidad_input != unidad2:
+                        print("Indique una unidad valida")
+                    elif unidad_input == unidad1:
+                        cantUnidad = int(input("Ingrese la cantidad de unidades requeridas: "))
+                        break
+                    elif unidad_input == unidad2:
+                        cantKilos = float(input("Ingrese la cantidad de kilos requeridos (10000 unidades = 1 Kg): "))
+                        break
+                    continue
+                    
+                while True:
+                    if unidad_input == unidad1:
+                        print(f"Usted ha elegido {cantUnidad} bulones con diametro: {diametro_input}mm / paso: {paso_input} / largo {largo_input} de pulgada / rosca {rosca_input}")
+                        break                   
+                    if unidad_input == unidad2:    
+                        print(f"Usted ha elegido {cantKilos}Kg de bulones con diametro: {diametro_input}mm / paso: {paso_input} / largo {largo_input} de pulgada / rosca {rosca_input}")
+                        break
+                while True:
+                    deseaContinuar = input("Confirmar si es correcto (Y para SI / N para NO)")
+                    if deseaContinuar == Y:
+                        cantUnidad = cuentaUnidad
+                        cantKilos = cuentaKilos
+                        print("Excelente")
+                        break    
+                    elif deseaContinuar == N:
+                        print("Volvamos a empezar")
+                    continue             
+                    
+            elif seleccion == 3:
+                print(f"""Usted esta comprando...
+                 Bulones por unidad: {cuentaUnidadTotal}
+                 Bulones por kilo: {cuentaKilosTotal}
+                 Cantidad total de bulones por unidad: {cuentaCantidadTotal}
+                 """)
+            
+
             elif seleccion == 4:
                 print(" ")
                 print("Usted ha elegido seguir comprando")
                 print(" ")
 
             elif seleccion == 5:
+                cantUnidad = 0
+                cantKilos = 0
+                cuentaUnidad = 0
+                cuentaUnidadTotal = 0
+                cuentaKilos = 0
+                cuentaKilosTotal = 0
+                cuentaCantidadTotal = 0
+                print(" ")
+                print("Se han borrado todos los datos ingresados")
+                print(" ")
+
+            elif seleccion == 6:
                 print(" ")
                 print("Ha salido de la compra con exito")
                 print(" ")
