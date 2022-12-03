f468564ab199ce81bd33d33009bdf30745fac0a6
Leandro <115804454+mangicl@users.noreply.github.com>
2022-12-03 02:24:56 -0300

    Actualizacion fecha calendario

--- Main/Index/Calendario.py
+++ Main/Index/Calendario.py
@@ -3,16 +3,15 @@ import calendar
 fecha = calendar.TextCalendar(
     calendar.SUNDAY)
 
-print(fecha.prmonth (2022, 11))
+print(fecha.prmonth (2022, 12))
 
 #segunda forma hecha para imprimir todo el año
-
 
 #Instancia de TextCalendar
 anio_entero = calendar.TextCalendar()
 
 #Elegimos el formato del año
-calendario_2022 = anio_entero.formatyear(2022)
+calendario_2023 = anio_entero.formatyear(2023)
 
 #Mostramos el resultado
-print(calendario_2022)
+print(calendario_2023)
